45016670e477842fcffe351d0f2104b9118d6824
Peter Smith <grognard.programs@gmail.com>
2023-09-04 18:15:32 -0400

    added dev mode

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export const placeholder = () => {
+  console.log("test");
   return 0;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, './dist')
+module.exports = (env, argv) => {
+  const isDev = argv.mode === 'development'
+
+  const entry = { index: './src/index.js' };
+  const plugins = [];
+
+  if(isDev) {
+    entry.devRunner = './src/utils/devRunner.js'
+
+    plugins.push(new HtmlWebpackPlugin())
+  }
+
+  return {
+    entry,
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, './dist'),
+      clean: true
+    },
+    plugins
   }
 };
