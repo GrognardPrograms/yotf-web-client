40275f59c5aa95c6381c3a177a730889fae7df4f
Peter Smith <grognard.programs@gmail.com>
2023-09-07 19:02:14 -0400

    Switch to perspective with some position changes

--- src/loadClient.ts
+++ src/loadClient.ts
@@ -1,4 +1,4 @@
-import { Camera, Scene, WebGLRenderer } from "three";
+import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
 
 import { buildAnimLoop, buildTextureDictionary } from "./graphics";
 import { buildWallMesh } from "./graphics/meshes";
@@ -10,7 +10,10 @@ export const loadClient = (parentDomElement) => {
   const renderer = new WebGLRenderer();
   renderer.setSize(window.innerHeight * 0.75, window.innerHeight * 0.75);
   const scene = new Scene();
-  const camera = new Camera();
+  const camera = new PerspectiveCamera(90, 1, 0.1, 100);
+
+  camera.position.y += 1;
+  camera.position.z += 2;
 
   const wallMesh = buildWallMesh(textureDictionary.get('faceTexture'), new Coordinate(0, 0, 0), new Rotation(0, 45, 0));
   scene.add(wallMesh);
